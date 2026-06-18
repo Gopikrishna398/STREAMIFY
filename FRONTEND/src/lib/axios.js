441eec181e06e59db95bf05cbd1fe78cbeb6d51b
Gopikrishna398 <gopikrishnachippalapalli@gmail.com>
2026-06-18 10:08:54 +0530

    feat: update axios configuration to use VITE_API_URL from environment variables

--- FRONTEND/src/lib/axios.js
+++ FRONTEND/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? `${window.location.protocol}//${window.location.hostname}:5001/api`
-    : "/api";
+    : import.meta.env.VITE_API_URL || "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
